c657fa9e90d7e634d84d81c78a6733de78f287a6
abhilashgd <anrewgd@gmail.com>
2022-01-26 00:58:07 +0530

    updated readme and hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -34,17 +34,111 @@ console.log(Object.keys(person));
 console.log(Object.values(person));
 console.log(JSON.stringify(person));
 
+// boolean
 var isAdult =true;
                 console.log(isAdult);
                 if (isAdult){ console.log("is adult")}
                 else{ console.log("is not dult")}
                 console.log(!true);
 
-
+// Arrays
 var names = [   "abhilash", 
                 "vikram", 
                 "chaitanya"];
+
 console.log(names);
 console.log("Index 0 - "+names[0]);
 console.log("Size = "+ names.length);
-            
+
+// arithametic operations
+var addition = 2+2;
+var subtraction = 2-2;
+var division = 10/2;
+var multiplicaiton = 10*2;
+var reminder = 10%2;
+var exponentation = 3 ** 4;
+console.log(addition);
+console.log(multiplicaiton);
+console.log(subtraction);
+console.log(division);
+console.log(reminder);
+console.log(exponentation);   
+
+//functions
+function addNumbers(number1, number2){
+    var addition = number1 + number2;
+    //console.log(addition);
+    return addition;
+}
+var result = addNumbers(10, 10);
+var result2 = addNumbers(23,27);
+console.log(result);
+console.log(result2);
+
+//built in functions
+var person1= {
+    name: "abhilash"
+}
+console.log(Object.values(person1));
+console.log(Object.keys(person1));
+console.log("abhilash".toUpperCase());
+console.log("abhilash".indexOf("l"));
+
+//loops
+for (var i = 0; i<=10;i++) {
+    console.log(i);
+}
+
+var names2 = [   "abhilash", 
+                "vikram", 
+                "chaitanya"];
+                //for i
+                for (var i = 0; i<names2.length;i++) {
+                    console.log(names[i]);
+                }
+                //for of
+                for (const name of names2) {
+                console.log(name);
+                    }
+                //for each
+                names2.forEach(function(name){
+                    console.log(name);
+                })
+                //while
+                var number5=0;
+                while(number5 < 5){
+                    console.log(number5);
+                    number5 = number5+1;
+                }
+                var condition =true;
+                while(condition){
+                    console.log("hello");
+                    condition=false;
+                }
+                var index =0;
+                while (index<names.length){
+                    console.log(names[index])
+                    index = index+1;
+                }
+                //do while
+                condition2 = true;
+                do {
+                        console.log("inside do while");
+                        condition2=false;
+                } while(condition2);
+
+                //break and continue
+                for(var i=0; i<=10;i++){
+                    if(i < 5){  continue; }
+                    console.log(i);
+                }
+
+                //Functions
+                function hello(){
+
+                }
+                hello =1
+                console.log(hello);
+
+                const hello2 = function (){
+                } //to avoid function getting assigned other values
